aca432c516a581b72fbbb1b0be64a6b3e03529dc
James Weber <j.gibweb@gmail.com>
2014-07-29 18:08:00 -0700

    90%

--- family_photo.rb
+++ family_photo.rb
@@ -1,18 +1,40 @@
-
-class FamilyPhotoArrangement
-  attr_accessor :array_of_family_members
+class FamilyPhotoArranger
+  attr_accessor :possible_arrangements
+  attr_reader :n_persons
 
   def initialize(n_persons)
-    self.array_of_family_members = (1..n_persons).to_a
+    @n_persons = n_persons
+    generate_all_possible_permutations
+    filter_permutations
+    p possible_arrangements.count
   end
 
-  def difference_in_ages
-
+  def generate_all_possible_permutations
+    self.possible_arrangements = (1..@n_persons).to_a.permutation.to_a
   end
 
-  def generate_all_combinations_of_family_members
-    array_of_family_members.shift
-    [1].product(array_of_family_members)
+  def filter_permutations
+    self.possible_arrangements.reject! do |permutation|
+      if check_left_end_of_the_row_has_the_1_year_old permutation
+        true #reject 
+      elsif check_differences_in_ages_of_every_two_family_members_does_not_exceed_2 permutation
+        true #reject
+      end
+    end
   end
 
+  def check_left_end_of_the_row_has_the_1_year_old permutation
+    permutation.first != 1
+  end
+
+  def check_differences_in_ages_of_every_two_family_members_does_not_exceed_2 permutation
+    permutation.each_cons(2) do |consecutive| 
+      return true if (consecutive.first - consecutive.last).abs > 2
+    end
+    false
+  end
+end
+
+ARGV.each do |a|
+  FamilyPhotoArranger.new(a.to_i) if a.to_i > 0 && a.to_i < 55 && a.respond_to?('to_i') == true
 end
---
+++ test_family_photo.rb
@@ -0,0 +1,71 @@
+
+require_relative "family_photo"
+require "test/unit"
+system("clear") # for easier to read console output
+
+class TestFamilyPhotoArranger < Test::Unit::TestCase
+  
+  def setup
+    puts "\n\n"
+  end
+
+  def teardown
+    puts "\n"
+  end
+
+  def test_when_n_equals_3
+    a = [1,2,3] 
+    b = [1,3,2] 
+    arry = [a,b]
+
+    n_persons = 3
+    family_arrangement = FamilyPhotoArranger.new(n_persons)
+    p family_arrangement.possible_arrangements.count
+
+    assert_equal( arry, family_arrangement.possible_arrangements)
+  end
+
+  def test_when_n_equals_4
+    a = [1,2,3,4] 
+    b = [1,2,4,3] 
+    c = [1,3,2,4] 
+    d = [1,3,4,2]
+    arry = [a,b,c,d]
+
+    n_persons = 4
+    family_arrangement = FamilyPhotoArranger.new(n_persons)
+    p family_arrangement.possible_arrangements.count
+
+    assert_equal( arry, family_arrangement.possible_arrangements)
+  end
+
+  def test_when_n_equals_5
+    a = [1,2,3,4,5] 
+    b = [1,2,3,5,4] 
+    c = [1,2,4,3,5] 
+    d = [1,2,4,5,3]
+    e = [1,3,2,4,5]
+    f = [1,3,5,4,2]
+    arry = [a,b,c,d,e,f]
+
+    n_persons = 5
+    family_arrangement = FamilyPhotoArranger.new(n_persons)
+    p family_arrangement.possible_arrangements.count
+
+    assert_equal( arry, family_arrangement.possible_arrangements)
+  end
+
+  def test_when_n_equals_6    
+    n_persons = 6
+    family_arrangement = FamilyPhotoArranger.new(n_persons)
+    p family_arrangement.possible_arrangements.count
+  end
+
+  def test_when_n_equals_7
+    n_persons = 7
+    family_arrangement = FamilyPhotoArranger.new(n_persons)
+    p family_arrangement.possible_arrangements.count
+  end
+
+
+end
